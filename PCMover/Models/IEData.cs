e91052523bc5c34bbab775f440785c51322995f8
strell0213 <ilya.chuprunov@gmail.com>
2025-11-18 20:40:25 +0500

    Архитектура папок. Создание класса с данными экспорта и импорта

---
+++ PCMover/Models/IEData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCMover.Models
+{
+    public class IEData
+    {
+        public string Title { get; set; }
+        public string Data { get; set; }
+        public string Type { get; set; }
+
+        IEData()
+        {
+
+        }
+
+        public void ImportData()
+        {
+
+        }
+
+        public void ExportDate()
+        {
+
+        }
+    }
+}
