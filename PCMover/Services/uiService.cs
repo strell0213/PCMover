1284abc3be3c9a7eaf44c793994f64c5707cb668
Morrdone <morrdonesk@gmail.com>
2025-12-10 22:00:42 +0500

    add function GetNotificationStart & GetPrivacyPersonalizationSettings & GetAdPersonalizationStart

--- PCMover/Services/uiService.cs
+++ PCMover/Services/uiService.cs
@@ -9,6 +9,7 @@ using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace PCMover.Services
 {
@@ -19,9 +20,9 @@ namespace PCMover.Services
         public List<IEData> ExportUI()
         {
             List<IEData> personalizationDatas = new List<IEData>();
-            //personalizationSettings(personalizationDatas);
-            //GetRegionSettings(systemDatas); //Настройки региона, языка, часовой пояс
-            //GetPrivacySettings(systemDatas); //Настройки конфидециальности
+            GetPrivacyPersonalizationSettings(personalizationDatas);
+            GetAdPersonalizationStart(personalizationDatas);
+            GetNotificationStart(personalizationDatas);
 
             return personalizationDatas;
         }
@@ -29,53 +30,110 @@ namespace PCMover.Services
         {
             IEData personalization = null;
 
-            int adWallpapers = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers", "BackgroundHistoryPath0", 0) ?? 0);
-            personalization = new IEData("SlideshowSourceDirectoriesSet", adWallpapers.ToString(), NAME_TYPE_DATA);
-            personalizationDatas.Add(personalization);
+            try
+            {
+                int adWallpapers = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers", "BackgroundHistoryPath0", 0) ?? 0);
+                personalization = new IEData("SlideshowSourceDirectoriesSet", adWallpapers.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
 
-            int adColor = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize", "Enabled", 0) ?? 0);
-            personalization = new IEData("BackgroundHistoryPath0", adColor.ToString(), NAME_TYPE_DATA);
-            personalizationDatas.Add(personalization);
+                int adColor = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize", "Enabled", 0) ?? 0);
+                personalization = new IEData("BackgroundHistoryPath0", adColor.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
 
-            //int adLockScreen  = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\PersonalizationCSP\LockScreenImagePath", "Enabled", 0) ?? 0);
-            //personalization = new IEData("AdverstingInfo", adLockScreen.ToString(), NAME_TYPE_DATA);
-            //personalizationDatas.Add(personalization);
+                int adTheme = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes", "Enabled", 0) ?? 0);
+                personalization = new IEData("WallpaperSetFromTheme", adTheme.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
 
-            int adTheme = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes", "Enabled", 0) ?? 0);
-            personalization = new IEData("WallpaperSetFromTheme", adTheme.ToString(), NAME_TYPE_DATA);
-            personalizationDatas.Add(personalization);
+                int adTask = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband", "Enabled", 0) ?? 0);
+                personalization = new IEData("FavoritesVersion", adTask.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);           
+            }
+            catch (Exception ex)
+            {
+                Logger log = new Logger("Error Privacy Settings", ex.Message, (logLevel)3);
+                log.Write();
+            }
 
-            //int adFonts = (int)(Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts", "Enabled", 0) ?? 0);
-            //personalization = new IEData("AdverstingInfo", adFonts.ToString(), NAME_TYPE_DATA);
-            //personalizationDatas.Add(personalization);
+}
+        public void GetNotificationStart(List<IEData> personalizationDatas)
+        {
+            IEData personalization = null;
 
-            //int adStart = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount", "Enabled", 0) ?? 0);
-            //personalization = new IEData("AdverstingInfo", adStart.ToString(), NAME_TYPE_DATA);
-            //personalizationDatas.Add(personalization);
+            try
+            {
+                int adToastEnabled = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\PushNotifications", "Enabled", 0) ?? 0);
+                personalization = new IEData("ToastEnabled", adToastEnabled.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
 
-            int adTask = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband", "Enabled", 0) ?? 0);
-            personalization = new IEData("FavoritesVersion", adTask.ToString(), NAME_TYPE_DATA);
-            personalizationDatas.Add(personalization);
+                int adNOC_GLOBAL_SETTING_ALLOW_TOASTS_ABOVE_LOCK = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\PushNotifications", "Enabled", 0) ?? 0);
+                personalization = new IEData("NOC_GLOBAL_SETTING_ALLOW_TOASTS_ABOVE_LOCK", adNOC_GLOBAL_SETTING_ALLOW_TOASTS_ABOVE_LOCK.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
+
+                int adNOC_GLOBAL_SETTING_ALLOW_TOASTS_SOUND = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\PushNotifications", "Enabled", 0) ?? 0);
+                personalization = new IEData("NOC_GLOBAL_SETTING_ALLOW_TOASTS_SOUND", adNOC_GLOBAL_SETTING_ALLOW_TOASTS_SOUND.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
+
+                int adSubscribedContentEnabled = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "Enabled", 0) ?? 0);
+                personalization = new IEData("SubscribedContent-338388Enabled", adSubscribedContentEnabled.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
+
+                int adSubscribedCon = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "Enabled", 0) ?? 0);
+                personalization = new IEData("SoftLandingEnabled", adSubscribedCon.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
+
+                int adSystemPaneSuggestionsEnabled = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "Enabled", 0) ?? 0);
+                personalization = new IEData("SystemPaneSuggestionsEnabled", adSystemPaneSuggestionsEnabled.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
+            }
+            catch (Exception ex)
+            {
+                Logger log = new Logger("Error Privacy Settings", ex.Message, (logLevel)3);
+                log.Write();
+            }
         }
-        //public void personalizationSettings(List<IEData> personalizationDatas)
-        //{
-        //    IEData personalization = null;
+        public void GetAdPersonalizationStart(List<IEData> personalizationDatas)
+        {
+            IEData personalization = null;
 
-        //    var background = System.Registry.GetValue();
-        //    personalization = new IEData("background", background.Background, NAME_TYPE_DATA);
-        //    personalizationDatas.Add(personalization);
+            try
+            {
+                int adStart_Expanded = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "Enabled", 0) ?? 0);
+                personalization = new IEData("Start_Grid_Expanded", adStart_Expanded.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
 
-        //    var region = System.Globalization.RegionInfo.CurrentRegion;
-        //    data = new IEData("Region", region.EnglishName, NAME_TYPE_DATA);
-        //    systemDatas.Add(data);
+                int adStart_ShowAppsList = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "Enabled", 0) ?? 0);
+                personalization = new IEData("Start_ShowAppsList", adStart_ShowAppsList.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
 
-        //    var ui = System.Globalization.CultureInfo.CurrentUICulture;
-        //    data = new IEData("Lang", ui.Name, NAME_TYPE_DATA);
-        //    systemDatas.Add(data);
+                int adStart_TrackAppInstall = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "Enabled", 0) ?? 0);
+                personalization = new IEData("Start_TrackAppInstall", adStart_TrackAppInstall.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
 
-        //    var tz = TimeZoneInfo.Local;
-        //    data = new IEData("TimeOffset", tz.BaseUtcOffset.Hours.ToString(), NAME_TYPE_DATA);
-        //    systemDatas.Add(data);
-        //}
+                int adStart_TrackUserAppUse = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "Enabled", 0) ?? 0);
+                personalization = new IEData("Start_TrackUserAppUse", adStart_TrackUserAppUse.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
+
+                int adStart_ShowSuggestions = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "Enabled", 0) ?? 0);
+                personalization = new IEData("Start_ShowSuggestions", adStart_ShowSuggestions.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
+
+                int adStart_Layout = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "Enabled", 0) ?? 0);
+                personalization = new IEData("adStart_Layout", adStart_Layout.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
+
+                int adStart_RecentDocsHistory = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "Enabled", 0) ?? 0);
+                personalization = new IEData("adStart_RecentDocsHistory", adStart_RecentDocsHistory.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
+
+                int adStart_AccountSettingsNotifications = (int)(Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "Enabled", 0) ?? 0);
+                personalization = new IEData("Start_AccountSettingsNotifications", adStart_AccountSettingsNotifications.ToString(), NAME_TYPE_DATA);
+                personalizationDatas.Add(personalization);
+            }
+            catch (Exception ex)
+            {
+                Logger log = new Logger("Error Privacy Settings", ex.Message, (logLevel)3);
+                log.Write();
+            }
+        }
     }
 }
